2e10c1b662339d470f0662e14f8a1f226d43ae5f
23f2005399 <23f2005399@ds.study.iitm.ac.in>
2026-06-12 14:41:08 +0530

    formatting and style fixes

--- frontend/static/js/user_components.js
+++ frontend/static/js/user_components.js
@@ -786,10 +786,6 @@ const TsUserLayout = {
           <input v-model="searchQuery" type="text" placeholder="Search treks…" @keyup.enter="goTab('explore')" />
         </div>
         <div class="topbar-actions">
-          <div class="topbar-icon-btn" @click="goTab('bookings')" title="Bookings">
-            <svg viewBox="0 0 24 24"><rect x="3" y="4" width="18" height="18" rx="2"/><line x1="16" y1="2" x2="16" y2="6"/><line x1="8" y1="2" x2="8" y2="6"/><line x1="3" y1="10" x2="21" y2="10"/></svg>
-            <span v-if="myBookings.filter(b=>b.status==='Booked').length" class="notif-dot"></span>
-          </div>
           <div class="topbar-user-pill" @click="goTab('profile')" title="Profile">
             <span class="topbar-pill-name">{{ profile.name ? profile.name.split(' ')[0] : 'Trekker' }}</span>
             <div class="topbar-pill-avatar">

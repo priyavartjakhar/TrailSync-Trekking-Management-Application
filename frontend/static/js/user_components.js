82be4999193c60807bde912211af4f778ce907a9
23f2005399 <23f2005399@ds.study.iitm.ac.in>
2026-06-12 14:51:50 +0530

    tweak styling on dashboard

--- frontend/static/js/user_components.js
+++ frontend/static/js/user_components.js
@@ -796,8 +796,8 @@ const TsUserLayout = {
       <!-- Sidebar footer -->
       <div class="sidebar-footer">
         <button class="btn-logout-sidebar" @click="handleLogout">
-          <svg viewBox="0 0 24 24"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/><polyline points="16 17 21 12 16 7"/><line x1="21" y1="12" x2="9" y2="12"/></svg>
           Sign Out
+          <svg viewBox="0 0 24 24"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/><polyline points="16 17 21 12 16 7"/><line x1="21" y1="12" x2="9" y2="12"/></svg>
         </button>
       </div>
     </aside>
